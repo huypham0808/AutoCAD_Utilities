2f7d6ddd6f102668ed4b3e1b3c3fae69bd2a867f
huypham0808 <phamngohoanghuy95@gmail.com>
2023-12-03 22:14:01 +0700

    FindReferTitleID

---
+++ FindReferTitleID/FindRefer.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Runtime.InteropServices;
+using Autodesk.AutoCAD.Runtime;
+using Autodesk.AutoCAD.ApplicationServices;
+using Autodesk.AutoCAD.DatabaseServices;
+using Autodesk.AutoCAD.Geometry;
+using Autodesk.AutoCAD.EditorInput;
+
+namespace FindReferTitleID
+{
+    public class FindRefer
+    {
+        [CommandMethod("CSS_FindRefer")]
+        public void CSS_FindRefer()
+        {
+            Document currentDocument = Application.DocumentManager.MdiActiveDocument;
+            Database currentDatabase = currentDocument.Database;
+            Editor editor = currentDocument.Editor;
+
+            // Prompt the user to select the block reference
+            PromptEntityOptions entityOptions = new PromptEntityOptions("\nSelect the block reference: ");
+            entityOptions.SetRejectMessage("\nInvalid selection. Please select a block reference.");
+            entityOptions.AddAllowedClass(typeof(BlockReference), true);
+
+            PromptEntityResult entityResult = editor.GetEntity(entityOptions);
+            if (entityResult.Status != PromptStatus.OK)
+                return;
+
+            ObjectId blockReferenceId = entityResult.ObjectId;
+
+            // Prompt the user to select the MText object
+            PromptEntityOptions mTextOptions = new PromptEntityOptions("\nSelect the MText object: ");
+            mTextOptions.SetRejectMessage("\nInvalid selection. Please select an MText object.");
+            mTextOptions.AddAllowedClass(typeof(MText), true);
+
+            PromptEntityResult mTextResult = editor.GetEntity(mTextOptions);
+            if (mTextResult.Status != PromptStatus.OK)
+                return;
+
+            ObjectId mTextObjectId = mTextResult.ObjectId;
+
+            // Start the transaction
+            using (Transaction transaction = currentDatabase.TransactionManager.StartTransaction())
+            {
+                // Open the block reference and its attribute collection
+                BlockReference blockReference = transaction.GetObject(blockReferenceId, OpenMode.ForRead) as BlockReference;
+                AttributeCollection attributeCollection = blockReference.AttributeCollection;
+
+                // Find the text attribute with tag "S1"
+                foreach (ObjectId attributeId in attributeCollection)
+                {
+                    AttributeReference attribute = transaction.GetObject(attributeId, OpenMode.ForRead) as AttributeReference;
+                    if (attribute != null && attribute.Tag.Equals("S1", StringComparison.OrdinalIgnoreCase))
+                    {
+                        // Create the field expression
+                        string fieldExpression = $"%<\\AcObjProp Object(%<\\_ObjId {mTextObjectId.Handle}>%).TextString>%";
+
+                        // Set the attribute text as the field expression
+                        attribute.UpgradeOpen();
+                        attribute.TextString = fieldExpression;
+                        attribute.DowngradeOpen();
+                        break;
+                    }
+                }
+                // Commit the transaction
+                transaction.Commit();
+            }
+        }
+    }
+}
---
+++ FindReferTitleID/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("FindReferTitleID")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Administrator")]
+[assembly: AssemblyProduct("FindReferTitleID")]
+[assembly: AssemblyCopyright("Copyright © Administrator 2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("e8936ea9-d2a8-45b1-b2b8-651240b3aa8a")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
